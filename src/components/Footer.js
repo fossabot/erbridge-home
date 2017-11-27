8bd699e67010777bf951f4fc5230ae9441df8788
erbridge <f@erbridge.co.uk>
2017-11-27 14:52:58 +0000

    Change my name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,18 @@ import ExternalLink from './ExternalLink';
 
 import './Footer.css';
 
-const Footer = () =>
+const Footer = () => (
   <div className="Footer">
     <div className="Footer__content">
       <div className="Footer__copyright">
-        <div className="Footer__copyright-line">
-          © 2015 - 2017 Felix Laurie von Massenbach
-        </div>
+        <div className="Footer__copyright-line">© 2015 - 2017 erbridge</div>
         <div className="Footer__copyright-line">
           <ExternalLink to="https://raw.githubusercontent.com/erbridge/erbridge-home/master/LICENSE">
             MIT
           </ExternalLink>{' '}
           (<ExternalLink to="https://github.com/erbridge/erbridge-home">
             code
-          </ExternalLink>) /{' '}
+          </ExternalLink>) |{' '}
           <ExternalLink to="http://creativecommons.org/licenses/by/3.0/">
             CC-BY
           </ExternalLink>{' '}
@@ -25,6 +23,7 @@ const Footer = () =>
         </div>
       </div>
     </div>
-  </div>;
+  </div>
+);
 
 export default Footer;
