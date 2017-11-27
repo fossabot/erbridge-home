3cf919206fe2f171822dfdd5f4dd02ae0b44c492
erbridge <f@erbridge.co.uk>
2017-11-27 15:59:13 +0000

    Add some style to the IndexPage

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,6 +1,9 @@
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+
+import './IndexPage.css';
 
 class IndexPage extends Component {
   static propTypes = {
@@ -19,11 +22,23 @@ class IndexPage extends Component {
     return (
       <div className="IndexPage">
         {routes.map((route, index) => (
-          <div key={route.name || index}>
-            <NavLink to={route.path} exact={route.exact}>
+          <NavLink
+            key={route.name || index}
+            className="IndexPage__link"
+            to={route.path}
+            exact={route.exact}
+          >
+            <div
+              className={classnames('IndexPage__link__title', {
+                'IndexPage__link__title--has-subtitle': route.date,
+              })}
+            >
               {route.title}
-            </NavLink>
-          </div>
+            </div>
+            {route.date && (
+              <div className="IndexPage__link__subtitle">{route.date}</div>
+            )}
+          </NavLink>
         ))}
       </div>
     );
