cf7c8266dd734960974d0970381771fd5347787f
erbridge <f@erbridge.co.uk>
2018-01-10 08:38:51 +0000

    Disable the service worker

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-registerServiceWorker();
+// FIXME: Implement a prompt to refresh when the content is updated before
+//        enabling the serivce worker.
+// registerServiceWorker();
