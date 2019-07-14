18890e7257702169f0c0fad8bb10c52daac38db9
erbridge <f@erbridge.co.uk>
2019-07-14 12:48:03 +0100

    Only mock pages in App tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,12 @@ import React from 'react';
 
 import App from './App';
 
+jest.mock('./pages/blog');
+jest.mock('./pages/fiction');
+jest.mock('./pages/games');
 jest.mock('./pages/home.mdx', () => ({ meta: {}, default: () => 'home.mdx' }));
+jest.mock('./pages/misc');
+jest.mock('./pages/web');
 
 it('renders correctly', () => {
   expect(shallow(<App />)).toMatchSnapshot();
--- src/setupTests.js
+++ src/setupTests.js
@@ -5,11 +5,6 @@ import mockFetch from 'jest-fetch-mock';
 Enzyme.configure({ adapter: new Adapter() });
 
 jest.mock('./assets');
-jest.mock('./pages/blog');
-jest.mock('./pages/fiction');
-jest.mock('./pages/games');
-jest.mock('./pages/misc');
-jest.mock('./pages/web');
 
 global.fetch = mockFetch;
 global.waitForNextTick = () =>
