891eb33faf83c8681b4af24150b75ed3823979df
erbridge <f@erbridge.co.uk>
2019-10-04 15:51:44 +0100

    Replace react-helmet with react-helmet-async

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Route } from 'react-router-dom';
 
 import routePropType from '../prop-types/routePropType';
@@ -13,16 +13,20 @@ class Head extends Component {
   render() {
     const { routes } = this.props;
 
-    return routes.map((route, index) => (
-      <Route key={route.name || index} path={route.path} exact>
-        <Helmet>
-          <title>
-            {route.title ? `${route.title} | ` : ''}
-            erbridge
-          </title>
-        </Helmet>
-      </Route>
-    ));
+    return (
+      <HelmetProvider>
+        {routes.map((route, index) => (
+          <Route key={route.name || index} path={route.path} exact>
+            <Helmet>
+              <title>
+                {route.title ? `${route.title} | ` : ''}
+                erbridge
+              </title>
+            </Helmet>
+          </Route>
+        ))}
+      </HelmetProvider>
+    );
   }
 }
 
